75eb0ce0d6d05a30bd17227464af2fc7e65f1f75
Kamiyada <you@example.comSonicSniper284@hotmail.com>
2021-08-16 05:30:01 -0500

    complete rover class

--- rover.js
+++ rover.js
@@ -1,11 +1,44 @@
 class Rover {
    // Write code here!
-   constructor(position, mode = "NORMAL", generatorWatts = 110) {
+   constructor(position, mode = "NORMAL" , generatorWatts = 110) {
       this.position = position;
       this.mode = mode;
       this.generatorWatts = generatorWatts;     
     }
+    receiveMessage() {
+      let response = {message:message.name , results:[]};
+  
+      for(let i = 0; i < message.commands.length; i++) {
+  
+          if (message.commands[i].commandType === "STATUS_CHECK") {
+            response.results.push({
+            completed: true , roverStatus: {mode: this.mode , generatorWatts: this.generatorWatts , position: this.position
+            }
+      });
+           
+   } 
+      else if (message.commands[i].commandType === "MOVE") {
+         if (this.mode === "LOW_POWER") {
+            response.results.push({
+            completed: false
+      });
+            } 
+      else {
+            response.results.push({
+            completed: true });
+            this.position = message.commands[i]; 
+         };
+        
+          } 
+      else if (message.commands[i].commandType === "MODE_CHANGE") {
+         response.results.push({
+         completed:true});
+         this.mode = message.commands[i]; 
+          }
+      }      
 
+      }
+   };
   
 
 
