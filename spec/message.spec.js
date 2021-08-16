dddc449e66b1df62cbb753a23e7261cbd6c82ffd
Kamiyada <you@example.comSonicSniper284@hotmail.com>
2021-08-16 07:20:53 -0500

    removed comment

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,9 +6,9 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
-  /*it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+  it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
     expect( function() { new Message();}).toThrow(new Error('Message name required.'));
-  });*/
+  });
 
   it("constructor sets name", function() {
     let message = new Message('New message!');
